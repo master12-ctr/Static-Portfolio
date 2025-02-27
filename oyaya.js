57b8bfc37d050e2825472904c5a0679c1f0a42b3
Tadese Worku Sete <tadese.worku@gohbetbank.com>
2025-02-27 10:20:19 +0300

    update to static portfolio

--- oyaya.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const plainPassword = 'oyaya'; // The password you want to hash
-const hashedPassword = bcrypt.hashSync(plainPassword, 10); // Hashing with 10 salt rounds
-
-console.log(hashedPassword);
